6507515ccd8afc5119592e5873ea08a75713a8db
hassanalyy <official.hassanalyy@gmail.com>
2023-03-14 12:03:30 +0500

    overview 17 n 18

---
+++ android/app/src/main/kotlin/com/example/overviews/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.overviews
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/overview17.dart
@@ -0,0 +1,350 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Overview17 extends StatelessWidget {
+  const Overview17({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(
+            'Overview',
+            style: TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          centerTitle: true,
+          bottom: PreferredSize(
+              child: Text(
+                "Week 02",
+                style: TextStyle(
+                  color: Color(0xffF60C87),
+                ),
+              ),
+              preferredSize: Size.zero),
+          leading: BackButton(color: Colors.black),
+          actions: [
+            Icon(Icons.search, color: Colors.black),
+            Icon(Icons.more_vert, color: Colors.black),
+          ],
+        ),
+        body: ListView(
+          children: [
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage("assets/images/profile1.png"),
+                    ),
+                    title: Text(
+                      'Juan dos Santos',
+                      style: TextStyle(
+                          color: Color(0xffF60C87),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
+                    subtitle: Text(
+                      'Ux/UI Designer  ',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Color(0xff000000),
+                      ),
+                    ),
+                    trailing: Icon(Icons.message,size: 32,),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+
+                    height: 50.64,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListTile(
+                      leading: Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: 50,
+                        color: Color(0xffF60C87),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text('Mon',style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w500),),
+                        ),
+                      ),
+                      title: Text(
+                        'Mc Donald’s  ',
+                        style: TextStyle(
+                            color: Color(0xff000000),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      trailing: Text('9 AM - 5 PM ',style:TextStyle(color: Color(0xff3F0071),fontWeight: FontWeight.bold,fontSize: 15),),
+
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+
+                    height: 50.64,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListTile(
+                      leading: Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: 50,
+                        color: Color(0xffF60C87),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text('Tue',style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w500),),
+                        ),
+                      ),
+                      title: Text(
+                        'Coffee Shop ',
+                        style: TextStyle(
+                            color: Color(0xff000000),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      trailing: Text('7 AM - 1 PM ',style:TextStyle(color: Color(0xff3F0071),fontWeight: FontWeight.bold,fontSize: 15),),
+
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+
+                    height: 50.64,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListTile(
+                      leading: Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: 50,
+                        color: Color(0xffF60C87),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text('Tue',style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w500),),
+                        ),
+                      ),
+                      title: Text(
+                        'Mc Donald’s  ',
+                        style: TextStyle(
+                            color: Color(0xff000000),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      trailing: Text('2 PM - 6 PM ',style:TextStyle(color: Color(0xff3F0071),fontWeight: FontWeight.bold,fontSize: 15),),
+
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+
+                    height: 50.64,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListTile(
+                      leading: Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: 50,
+                        color: Color(0xffF60C87),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text('Wed',style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w500),),
+                        ),
+                      ),
+                      title: Text(
+                        'Mc Donald’s  ',
+                        style: TextStyle(
+                            color: Color(0xff000000),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      trailing: Text('9 AM - 5 PM ',style:TextStyle(color: Color(0xff3F0071),fontWeight: FontWeight.bold,fontSize: 15),),
+
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+
+                    height: 50.64,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListTile(
+                      leading: Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: 50,
+                        color: Color(0xffF60C87),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text('Thu',style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w500),),
+                        ),
+                      ),
+                      title: Text(
+                        'Coffee Shop ',
+                        style: TextStyle(
+                            color: Color(0xff000000),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      trailing: Text('7 AM - 1 PM ',style:TextStyle(color: Color(0xff3F0071),fontWeight: FontWeight.bold,fontSize: 15),),
+
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+
+                    height: 50.64,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListTile(
+                      leading: Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: 50,
+                        color: Color(0xffF60C87),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text('Thu',style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w500),),
+                        ),
+                      ),
+                      title: Text(
+                        'Mc Donald’s  ',
+                        style: TextStyle(
+                            color: Color(0xff000000),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      trailing: Text('2 PM - 6 PM ',style:TextStyle(color: Color(0xff3F0071),fontWeight: FontWeight.bold,fontSize: 15),),
+
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+
+                    height: 50.64,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListTile(
+                      leading: Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: 50,
+                        color: Color(0xffF60C87),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text('Fri',style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w500),),
+                        ),
+                      ),
+                      title: Text(
+                        'Mc Donald’s ',
+                        style: TextStyle(
+                            color: Color(0xff000000),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      trailing: Text('9 AM - 5 PM ',style:TextStyle(color: Color(0xff3F0071),fontWeight: FontWeight.bold,fontSize: 15),),
+
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+
+                    height: 50.64,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListTile(
+                      leading: Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: 50,
+                        color: Color(0xffF60C87),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text('Sat',style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w500),),
+                        ),
+                      ),
+                      title: Text(
+                        'Coffee Shop',
+                        style: TextStyle(
+                            color: Color(0xff000000),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      trailing: Text('7 AM - 1 PM ',style:TextStyle(color: Color(0xff3F0071),fontWeight: FontWeight.bold,fontSize: 15),),
+
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+
+                    height: 50.64,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListTile(
+                      leading: Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: 50,
+                        color: Color(0xffF60C87),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text('Sat',style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w500),),
+                        ),
+                      ),
+                      title: Text(
+                        'Mc Donald’s ',
+                        style: TextStyle(
+                            color: Color(0xff000000),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      trailing: Text('2 PM - 6 PM ',style:TextStyle(color: Color(0xff3F0071),fontWeight: FontWeight.bold,fontSize: 15),),
+
+                    ),
+                  ),
+                ),
+              ],
+
+            ),
+            BottomNavigationBar(
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home,color: Color(0xff797D7F)),
+                  label: 'Home',
+                  backgroundColor: Colors.white,
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.calendar_month,color: Color(0xff797D7F)),
+                  label: 'Calender',
+                  //   backgroundColor: Colors.white,
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.message_outlined,color:Color(0xff797D7F),),
+                  label: 'Messages',
+                  //   backgroundColor: Colors.white,
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.edit_document,color: Color(0xff797D7F),),
+                  label: 'Documents',
+                  //  backgroundColor: Colors.white,
+                ),
+              ],
+
+            ),
+          ],
+
+        ),
+      ),
+    );
+  }
+}
